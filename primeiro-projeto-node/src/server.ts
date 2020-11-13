424b7e0bd16ae3643eaa2c27a620eb5d49023752
Emerson Freitas <emersonfreitasn@gmail.com>
2020-11-13 12:40:01 -0500

    registration of schedules and standardization of the project

--- primeiro-projeto-node/src/server.ts
+++ primeiro-projeto-node/src/server.ts
@@ -1,19 +1,11 @@
-import express, { request, response } from 'express'
+import express from 'express'
+
+import routes from './routes'
 
 const app = express()
 
 app.use(express.json())
-
-app.post('/users', (request, response) => {
-  const { name, email } = request.body
-
-  const user = {
-    name,
-    email
-  }
-
-  return response.json(user)
-})
+app.use(routes)
 
 app.listen(3333, () => {
   console.log('✔ Server started on port 3333!')
